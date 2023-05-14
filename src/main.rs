9d9715ebcf638597519777f9a1476313f086f496
Vlad Onis <vladonis.biz+opensource@gmail.com>
2023-05-14 22:20:18 +0300

    Initial implementation of the service layer

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 mod model;
+mod service;
 mod storage;
 mod ui;
 
--- src/model/command.rs
+++ src/model/command.rs
@@ -1,7 +1,29 @@
 use sqlx::FromRow;
 
+// todo: introduce alias for commands
+
 #[derive(Clone, FromRow, Debug)]
 pub struct Command {
-    pub command: String,
+    pub alias: String,
     pub executable: String,
+    pub command: String,
+    pub description: Option<String>,
+}
+
+impl Command {
+    pub fn new(
+        executable: String,
+        command: String,
+        alias: String,
+        description: Option<String>,
+    ) -> Command {
+        // todo: validate that the executable is installed
+        // todo: Perform a dryrun to ensure command is correct.
+        Command {
+            alias,
+            executable,
+            command,
+            description,
+        }
+    }
 }
